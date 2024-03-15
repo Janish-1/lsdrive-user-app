172f30b016d3999cc3b7cab6528c56f1cfe6327c
Janish-1 <janish.pancholi11@gmail.com>
2024-03-15 11:14:43 +0530

    Removed the Front Files

--- LS Drive final/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("openPopupButton").addEventListener("click", function() {
-  document.getElementById("popupContainer").style.display = "block";
-});
-
-// Function to close the popup
-function closePopup() {
-  document.getElementById("popupContainer").style.display = "none";
-}
